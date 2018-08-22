5da703ab9651e9f1e857c5428c9da85d53a08b61
joaohenriquef <joaohenriquef@poli.ufrj.br>
2018-08-22 02:42:56 -0300

    Fix EEPROM functionality

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,13 @@
 #include <SimpleKalmanFilter.h>
 
 #define SQUIB_PIN 10
+#define EEPROM_DATA_TYPE float
 #define DEBUG_SERIAL_TIMEOUT_MILLIS 10000
-#define ALTITUDE_IS_LOWER_THRESHOLD 10 // Recovery System
-#define MIN_ALT_FOR_APOGEE_DETECTION 10
+#define ALTITUDE_IS_LOWER_THRESHOLD 10 // Number of consecutive lower values for altitude to detect apogee
+#define MIN_ALT_FOR_APOGEE_DETECTION 0
 #define ALTITUDE_DATA_POINTS 30
 #define TELEMETRY_DATA_POINTS 4
+
 #define EEPROM_BYTES_NUMBER 8
 
 BMP085 barometer;
@@ -38,27 +40,29 @@ SimpleKalmanFilter pressureKalmanFilter(1, 1, 0.01);
 void updateBarometer(void);
 void updateMatrix(float *vetor);
 
-float pressure, filteredPressure, temperature, referenceAltitude, altitude;
+float pressure, filteredPressure;
+float temperature;
+float referenceAltitude, altitude, apogee = 0;
 float altitudeData[ALTITUDE_DATA_POINTS + 1];
 float parachuteReleaseTime = 0;
 
 int altitudeIsLowerCounter = 0;
-int apogee = 0;
-unsigned int eepromMemLocation = 0;
+unsigned int eepromCurrentAddr;
 
 bool parachuteReleased = false;
 
 void setup()
 {
     float lastApogee, lastReferenceAltitude;
-    
+    unsigned int eepromLastApogeeAddr, eepromLastRefAltAddr;
+
     pinMode(SQUIB_PIN, OUTPUT);
-    
+
     // Join I2C bus (I2Cdev library doesn't do this automatically)
     Wire.begin();
     // 38400 default because it works as well at 8MHz as it does at 16MHz
     Serial.begin(115200);
-    
+
     // Initialize devices
     Serial.println("Initializing I2C devices...");
     barometer.initialize();
@@ -78,20 +82,42 @@ void setup()
         updateBarometer();
         updateMatrix(altitudeData);
     }
-    referenceAltitude = altitude; //altitude correction factor
+    referenceAltitude = altitude; // Altitude correction factor
+    apogee = referenceAltitude;
 
-    EEPROM.get(0, eepromMemLocation); //gets current EEPROM memory adress to be used
-    eepromMemLocation += EEPROM_BYTES_NUMBER; //changes to new write location
+    EEPROM.get(0, eepromCurrentAddr); // Gets current EEPROM memory adress to be used
 
-        //checks if at the end of memory restart count
-        if (eepromMemLocation + EEPROM_BYTES_NUMBER > EEPROM.length())
-        {
-            eepromMemLocation = 0;
-        }
-        EEPROM.put(1, eepromMemLocation); //updates memory location tracker
+    eepromLastApogeeAddr = eepromCurrentAddr - sizeof(EEPROM_DATA_TYPE);
+    eepromLastRefAltAddr = eepromLastApogeeAddr - sizeof(EEPROM_DATA_TYPE);
 
-        EEPROM.get(eepromMemLocation, lastApogee);
-        EEPROM.get(eepromMemLocation + 4, lastReferenceAltitude);
+    // Address overflow corrections
+    if (eepromLastApogeeAddr > EEPROM.length())
+    {
+        eepromLastApogeeAddr = EEPROM.length() - (sizeof(EEPROM_DATA_TYPE));
+        eepromLastRefAltAddr = eepromLastApogeeAddr - sizeof(EEPROM_DATA_TYPE);
+    }
+    if (eepromLastRefAltAddr > EEPROM.length())
+    {
+        eepromLastRefAltAddr = EEPROM.length() - (sizeof(EEPROM_DATA_TYPE));
+    }
+
+    // Restart counter when memory ends
+    if (eepromCurrentAddr + EEPROM_BYTES_NUMBER > EEPROM.length())
+    {
+        eepromCurrentAddr = 0;
+        EEPROM.put(0, eepromCurrentAddr); // Updates memory location tracker
+    }
+
+    EEPROM.get(eepromLastApogeeAddr, lastApogee);
+    EEPROM.get(eepromLastRefAltAddr, lastReferenceAltitude);
+
+    // EEPROM DEBUG
+    // Serial.print("EEPROM POINTER: ");
+    // Serial.println(eepromCurrentAddr);
+    // Serial.print("LAST APOGEE POINTER: ");
+    // Serial.println(eepromLastApogeeAddr);
+    // Serial.print("LAST REF ALT POINTER: ");
+    // Serial.println(eepromLastRefAltAddr);
 
     Serial.print("Last recorded uncorrected Apogee: ");
     Serial.println(lastApogee);
@@ -99,32 +125,34 @@ void setup()
     Serial.println(lastReferenceAltitude);
     Serial.print("Last recorded corrected Apogee: ");
     Serial.println(lastApogee - lastReferenceAltitude);
-
-
 }
 
 void loop()
 {
-    updateBarometer();     // Updates barometer information
-    
+    updateBarometer();          // Updates barometer information
+    updateMatrix(altitudeData); // Updates the altitude data matrix
+
     if (apogee < altitude) // Checks if the most recent altitude data is higher than the current apogee value
     {
         apogee = altitude; // If it is, updates apogee
     }
 
     // Display measured values if appropriate
-    Serial.print("T/P/A\t");
-    Serial.print(temperature);
-    Serial.print("\t");
-    Serial.print(pressure);
-    Serial.print("\t");
-    Serial.print("Barometer altitude: "); // Raw altitude
-    Serial.println(altitude - referenceAltitude);
+    if (!parachuteReleased)
+    {
+        Serial.print("T/P/A/Apogee\t");
+        Serial.print(temperature);
+        Serial.print("\t");
+        Serial.print(pressure);
+        Serial.print("\t");
+        Serial.print(apogee);
+        Serial.print("\t");
+        Serial.print("Barometer altitude: "); // Raw altitude
+        Serial.println(altitude - referenceAltitude);
+    }
 
     /* RECOVERY CODE */
-    updateMatrix(altitudeData); // Updates the altitude data matrix
-
-    Serial.println(parachuteReleased); // Debugging
+    //    Serial.println(parachuteReleased); // Debugging
 
     if (parachuteReleased == false)
     {
@@ -139,25 +167,27 @@ void loop()
             altitudeIsLowerCounter = 0;
         }
 
-        // If ALTITUDE_IS_LOWER_THRESHOLD is reached write to pin
+        // If ALTITUDE_IS_LOWER_THRESHOLD is reached, deploy parachute
         if (altitudeIsLowerCounter >= ALTITUDE_IS_LOWER_THRESHOLD && altitude - referenceAltitude > MIN_ALT_FOR_APOGEE_DETECTION)
         {
             digitalWrite(SQUIB_PIN, HIGH);
-            parachuteReleaseTime = millis() / 1000.0;
-            Serial.println("Parachute Release!");
             parachuteReleased = true;
-
-            if (!Serial) // If pc is not connected write info
-            {
-                EEPROM.put(eepromMemLocation, apogee);
-                EEPROM.put(eepromMemLocation + 4, referenceAltitude);
-            }
+            parachuteReleaseTime = millis() / 1000.0;
+            
+            Serial.println("Parachute released! Apogee/RefAlt");
+            Serial.println(apogee);
+            Serial.println(referenceAltitude);
+            
+            // EEPROM DEBUG
+            // Serial.print("SAVED REF ALT AT ADDR: ");
+            // Serial.println(eepromCurrentAddr);
+            // Serial.print("SAVED APOGEE AT ADDR: ");
+            // Serial.println(eepromCurrentAddr + sizeof(EEPROM_DATA_TYPE));
+            
+            EEPROM.put(eepromCurrentAddr, referenceAltitude);
+            EEPROM.put(eepromCurrentAddr + sizeof(EEPROM_DATA_TYPE), apogee);
+            EEPROM.put(0, eepromCurrentAddr + 2*sizeof(EEPROM_DATA_TYPE));
         }
-    }
-
-    for (int k = 0; k <= ALTITUDE_DATA_POINTS; k++) // Debugging
-    {
-        Serial.println(altitudeData[k]);
     }
 
     /*TELEMETRY CODE*/
@@ -166,9 +196,6 @@ void loop()
     telemetryVector[1] = pressure;
     telemetryVector[2] = filteredPressure;
     telemetryVector[3] = parachuteReleaseTime;
-
-    // Delay 100 msec to allow visually parsing blink and any serial output
-    delay(500);
 }
 
 void updateMatrix(float *vetor)
